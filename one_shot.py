adcb05a3374e3a1ec77c3b8a0f31838bf3438df7
Mayuresh Kamble <mackam30@gmail.com>
2026-01-29 19:50:07 +0530

    back to basics

---
+++ one_shot.py
@@ -0,0 +1,20 @@
+# name = "Tony Stark"
+# age = 51
+# isGenius = True
+
+# print(name, age, isGenius)
+
+def person():
+    name = "Tony Stark"
+    age = 51
+    isGenius = True
+    # return(name , age , isGenius)
+    print(name, age, isGenius)
+
+person()
+
+def secretName():
+    secret = input("What is ur secret name: ")
+    print(secret)
+
+secretName()
