3668084beeb229c95b48a6e895c2386cdb424781
Mayuresh Kamble <mackam30@gmail.com>
2026-02-03 11:53:08 +0530

    more question from hackerrank

--- one_shot.py
+++ one_shot.py
@@ -104,19 +104,24 @@
 
 # leap year program
 
-def is_leap(year):
-    leap = False
+# def is_leap(year):
+#     leap = False
     
-    if year % 400 == 0:
-        leap = True
-    elif year % 100 == 0:
-        leap = False
-    elif year % 4 == 0:
-        leap = True
-    else:
-        leap = False
-    return leap
+#     if year % 400 == 0:
+#         leap = True
+#     elif year % 100 == 0:
+#         leap = False
+#     elif year % 4 == 0:
+#         leap = True
+#     else:
+#         leap = False
+#     return leap
 
-year = int(input())
-print(is_leap(year))
+# year = int(input())
+# print(is_leap(year))
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n + 1):
+        print(i, end="")
 
