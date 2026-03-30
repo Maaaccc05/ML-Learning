7bffe465797cb57c2a852ad44339aec381c083c9
Maaaccc05 <mackam30@gmail.com>
2026-03-30 13:17:00 +0530

    done with image cropping

--- OpenCV/Phase-2-Image resizing/cropped.py
+++ OpenCV/Phase-2-Image resizing/cropped.py
@@ -2,5 +2,11 @@ import cv2
 image = cv2.imread("OpenCV/minato-namikaze-3840x2160-24353.png")
 
 if image is not None:
-    cropped = image[100:200, 50:150]
-    
+    cropped = image[50:300, 60:200]
+
+    cv2.imshow("Cropped Image", cropped)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+# syntax for image cropping --> image[start y: end y, start x: end x]
