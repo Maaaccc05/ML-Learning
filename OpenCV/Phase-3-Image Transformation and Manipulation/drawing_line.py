0dc200f4bbdd4e45aed37f53f2c160cec87cbd7a
Maaaccc05 <mackam30@gmail.com>
2026-04-02 20:51:45 +0530

    put text on image

--- OpenCV/Phase-3-Image Transformation and Manipulation/drawing_line.py
+++ OpenCV/Phase-3-Image Transformation and Manipulation/drawing_line.py
@@ -39,17 +39,32 @@ image = cv2.imread("OpenCV/dmxffni837f1xrj8pki9xgrl.jpg")
 # Drawing Circle
 # Syntax: cv2.circle(imgae, center, radius, color, thickness)
 
+# if image is None:
+#     print("Not found")
+
+# else:
+#     print("Image Loaded")
+#     center = (400, 200)
+#     radius = 200
+#     color = (0, 0, 255)
+#     thickness = 3  # thickness can be -1 if you want to fill your cicle
+
+#     cv2.circle(image, center, radius, color, thickness)
+
+#     cv2.imshow("Circle", image)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+
+# Adding Text
+# syntanx: cv2.putText(image, text, org, font, fontScale, color, thickness)
+
 if image is None:
     print("Not found")
 
 else:
     print("Image Loaded")
-    center = (400, 200)
-    radius = 200
-    color = (0, 0, 255)
-    thickness = 3
 
-    cv2.circle(image, center, radius, color, thickness)
+    cv2.putText(image, "Hello OpenCV", (250, 400), cv2.FONT_HERSHEY_COMPLEX, 1.3, (0, 200, 255))
 
     cv2.imshow("Rectangle", image)
     cv2.waitKey(0)
