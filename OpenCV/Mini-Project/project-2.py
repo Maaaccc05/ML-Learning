79c119304875eaa19d7641f5a98e4a5b2e593dd0
Maaaccc05 <mackam30@gmail.com>
2026-04-03 23:37:04 +0530

    working on mini-project

--- OpenCV/Mini-Project/project-2.py
+++ OpenCV/Mini-Project/project-2.py
@@ -4,6 +4,13 @@ file_path = input("Enter Your file location")
 
 image = cv2.imread(file_path)
 
-cv2.imshow("Image", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+choice = input("1.Type 'line' for drawing line on image\n2.Type 'rectangle' to draw rectangle on image\n3.Type 'circle' to draw circle on image\n4.Type 'text' to add tect on image ").lower()
+
+if choice == 'line':
+    pt1 = input("Enter the point from where you want line")
+    pt2 = input("Enter the point till where you want line")
+    color = input("Choose color in format of (0, 0, 0)")
+    thickness = input("Choose thickness")
+    cv2.line(image, pt1,pt2, color, thickness)
+else:
+    print("Wrong text")
--- OpenCV/Phase-3-Image Transformation and Manipulation/drawing.py
+++ OpenCV/Phase-3-Image Transformation and Manipulation/drawing.py
@@ -1,21 +1,21 @@
 import cv2
 image = cv2.imread("OpenCV/dmxffni837f1xrj8pki9xgrl.jpg")
 
-# if image is None:
-#     print("Not found")
+if image is None:
+    print("Not found")
 
-# else:
-#     print("Image Loaded")
-#     pt1 = (50,50)
-#     pt2 = (400, 55)
-#     color = (0, 0, 255)
-#     thickness = 5
+else:
+    print("Image Loaded")
+    pt1 = (50,50)
+    pt2 = (400, 55)
+    color = (0, 0, 255)
+    thickness = 5
 
-#     cv2.line(image, pt1,pt2, color, thickness)
+    cv2.line(image, pt1,pt2, color, thickness)
 
-#     cv2.imshow("Line image", image)
-#     cv2.waitKey(0)
-#     cv2.destroyAllWindows()
+    cv2.imshow("Line image", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 # Drawing rectangle 
 # syntax: cv2.rectangle(image, pt1, pt2, color, thickness)
@@ -36,8 +36,8 @@ image = cv2.imread("OpenCV/dmxffni837f1xrj8pki9xgrl.jpg")
 #     cv2.waitKey(0)
 #     cv2.destroyAllWindows()
 
-# Drawing Circle
-# Syntax: cv2.circle(imgae, center, radius, color, thickness)
+# # Drawing Circle
+# # Syntax: cv2.circle(imgae, center, radius, color, thickness)
 
 # if image is None:
 #     print("Not found")
@@ -55,17 +55,17 @@ image = cv2.imread("OpenCV/dmxffni837f1xrj8pki9xgrl.jpg")
 #     cv2.waitKey(0)
 #     cv2.destroyAllWindows()
 
-# Adding Text
-# syntanx: cv2.putText(image, text, org, font, fontScale, color, thickness)
+# # Adding Text
+# # syntanx: cv2.putText(image, text, org, font, fontScale, color, thickness)
 
-if image is None:
-    print("Not found")
+# if image is None:
+#     print("Not found")
 
-else:
-    print("Image Loaded")
+# else:
+#     print("Image Loaded")
 
-    cv2.putText(image, "Hello OpenCV", (250, 400), cv2.FONT_HERSHEY_COMPLEX, 1.3, (0, 200, 255))
+#     cv2.putText(image, "Hello OpenCV", (250, 400), cv2.FONT_HERSHEY_COMPLEX, 1.3, (0, 200, 255))
 
-    cv2.imshow("Rectangle", image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+#     cv2.imshow("Rectangle", image)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
