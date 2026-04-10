b5de0d3f3557061ae0af960057e87e801729f6a3
Maaaccc05 <mackam30@gmail.com>
2026-04-10 23:47:53 +0530

    Video capturing done

--- OpenCV/Phase-4-Vdo and Webcam/vdo-saving.py
+++ OpenCV/Phase-4-Vdo and Webcam/vdo-saving.py
@@ -7,3 +7,23 @@ camera = cv2.VideoCapture(0)
 frame_width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
+codec = cv2.VideoWriter_fourcc(*'mp4v') # for mp4 format, you can also use 'XVID' for avi format
+recorded = cv2.VideoWriter("myvideo.mp4", codec, 24, (frame_width, frame_height))
+
+while True:
+    success, image = camera.read()
+    
+    if not success:
+        break
+
+    recorded.write(image)
+    cv2.imshow("Recorded", image)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        print("Quitting...")
+        break
+
+camera.release()
+recorded.release()
+
+cv2.destroyAllWindows()
