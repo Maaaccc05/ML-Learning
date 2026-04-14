90d35acac0524402aafc1781c7b000c120d09073
Maaaccc05 <mackam30@gmail.com>
2026-04-14 11:59:08 +0530

    implemented median  blurr on image

--- OpenCV/Mini-Project/project-3.py
+++ OpenCV/Mini-Project/project-3.py
@@ -3,7 +3,7 @@ import subprocess
 
 camera = cv2.VideoCapture(0)
 
-Choice = int(input("1 for Capture and save Video in directory\n2 for Only Capture Video\n 3 for Capture video and open in VLC media player"))
+Choice = int(input("1 for Capture and save Video in directory\n2 for Only Capture Video\n 3 for Capture video and open in VLC media player\nEnter Your CHoice: "))
 
 def all_vdo_related():
     frame_width = int(camera.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -43,25 +43,25 @@ def all_vdo_related():
 
         camera.release()
         cv2.destroyAllWindows()
-    elif Choice == 3:
-        recorded = cv2.VideoWriter("myvideo.avi", codec, 24, (frame_width, frame_height))
+    # elif Choice == 3:
+    #     recorded = cv2.VideoWriter("myvideo.avi", codec, 24, (frame_width, frame_height))
 
-        while True:
-            success, image = camera.read()
+    #     while True:
+    #         success, image = camera.read()
 
-            if not success:
-                break
+    #         if not success:
+    #             break
         
-            recorded.write(image)
-            cv2.imshow("Soon will be Saving", image)
+    #         recorded.write(image)
+    #         cv2.imshow("Soon will be Saving", image)
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                print("Quitting...")
-                break
-        camera.release()
-        recorded.release()
-        cv2.destroyAllWindows()
-        subprocess.Popen(["/usr/bin/vlc", "myvideo.avi"])
+    #         if cv2.waitKey(1) & 0xFF == ord('q'):
+    #             print("Quitting...")
+    #             break
+    #     camera.release()
+    #     recorded.release()
+    #     cv2.destroyAllWindows()
+    #     subprocess.Popen(["/usr/bin/vlc", "myvideo.avi"])
     else:
         print("wrong choice")
 
