c2aa8db9f9a3aa092c9c42a19dab7c2eeea94302
Maaaccc05 <mackam30@gmail.com>
2026-04-14 13:27:15 +0530

    phase 5 completed image sharpening, median blur & Gaussian Blur

--- OpenCV/Phase-5-Image Filetering and  Blurring/image_blurring.py
+++ OpenCV/Phase-5-Image Filetering and  Blurring/image_blurring.py
@@ -1,6 +1,7 @@
 # Gaussian Blur Syntax: cv2.GaussianBlur(src, (kernel_size, kernel_size), sigma)
 
 import cv2
+import numpy as np
 
 # image = cv2.imread("OpenCV/landscape-lake-mountain-clear-nature-wallpaper-preview.jpg")
 
@@ -16,17 +17,31 @@ import cv2
 
 # Median Blur Syntax : cv2.medianBlur(src, kernel_size)
 
-img = cv2.imread("OpenCV/Blur-img2.jpeg")
+# img = cv2.imread("OpenCV/Blur-img2.jpeg")
 
-med_blur = cv2.medianBlur(img, 3)
+# med_blur = cv2.medianBlur(img, 3)
 
-cv2.imshow("Original",img)
-cv2.imshow("MedianBlur", med_blur)
+# cv2.imshow("Original",img)
+# cv2.imshow("MedianBlur", med_blur)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 
 
 # Sharpening an Image Syntax: cv2.filter2D(src, ddepth, kernel)
 
+sharpenImg = cv2.imread("OpenCV/37054504-089d7a00-214d-11e8-8982-ca836f7a4460.jpg")
+
+sharpen_kernel = np.array([
+    [0, -1, 0],
+    [-1, 5, -1],
+    [0, -1, 0]
+])
+
+sharpened = cv2.filter2D(sharpenImg, -1, sharpen_kernel)
+
+cv2.imshow("Original", sharpenImg)
+cv2.imshow("Sharpened", sharpened)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
