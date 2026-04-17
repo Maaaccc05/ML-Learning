a15726fb3204ba4d140c7885fb27c2a525e352e3
Maaaccc05 <mackam30@gmail.com>
2026-04-17 21:25:51 +0530

    Contour Detection on triangle is done

--- OpenCV/Phase-7-Contours and Shape Detection/countour.py
+++ OpenCV/Phase-7-Contours and Shape Detection/countour.py
@@ -3,3 +3,19 @@
     Syntax for Draw Contoirs: cv2.drawContours(img, contours, contour_index, color, thickness)
 
 """
+
+import cv2
+
+img = cv2.imread("OpenCV/Phase-7-Contours and Shape Detection/triangle.png")
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+_, thresh = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY)
+
+# Find Contours
+
+contours, hirearchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+cv2.drawContours(img, contours, -1, (0,255,0), 3)
+
+cv2.imshow("Contours", img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
