4795a59f9291b7d50be318ff7e2209b46d66050d
Maaaccc05 <mackam30@gmail.com>
2026-04-19 13:22:32 +0530

    face detected by pretrained model of capturing face

--- OpenCV/Phase-8-Face & Object Detection/index.py
+++ OpenCV/Phase-8-Face & Object Detection/index.py
@@ -9,4 +9,15 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale()
+    faces = face_cascade.detectMultiScale(gray, 1.05, 5) # ---> 1.1 is ---> Reduce image size by 10% at each step, 5 is ---> How many nearby detections are needed to confirm a face
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
+
+    cv2.imshow("Face Detection", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
