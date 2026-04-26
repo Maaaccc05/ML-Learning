63b87484ee0abc5ad6fdc75ff5e79f098d21f56b
Maaaccc05 <mackam30@gmail.com>
2026-04-26 20:58:55 +0530

    Done making color detection it's detecting green color it's only suitable for only 3 colors for now green blue and yellow

--- OpenCV/Mini-Project/colorDetection/color_detection.py
+++ OpenCV/Mini-Project/colorDetection/color_detection.py
@@ -1,21 +1,31 @@
 import cv2
 from util import get_limits
+from PIL import Image
 
 
-yellow = [0, 255, 255]
-cap = cv2.VideoCapture(0)
+Green = [0, 255, 0]
+cap = cv2.VideoCapture("http://192.168.0.114:4747/video")
 while True:
     ret, frame = cap.read()
 
     hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lowerLimit, upperLimit = get_limits(yellow)
-    mask = cv2.inRange()
+    lowerLimit, upperLimit = get_limits(color=Green)
+    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
 
+    mask_ = Image.fromarray(mask)
+
+    boundbox = mask_.getbbox()
+
+    if boundbox is not None:
+        x1, y1, x2, y2 = boundbox
+
+        frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 4)
+        
     cv2.imshow("Vid", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-cap.release
+cap.release()
 cv2.destroyAllWindows()
